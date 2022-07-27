0ed0d240e6e1e26b430c4d2bf119b0a2fb112210
Roman <mazaxaka.tyt@gmail.com>
2022-07-27 12:51:57 +0300

    add like-active

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let addToCartBtns = document.querySelectorAll(".add-to-cart-btn");
 // };
 
 // addToCartBtns.addEventListener("click", function () {
-//   alert("helloo world");
+//   alert("hello world");
 // });
 
 // for (let i = 0; i < addToCartBtns.length; i++) {
@@ -21,7 +21,14 @@ let addToCartBtns = document.querySelectorAll(".add-to-cart-btn");
 addToCartBtns.forEach((item) =>
   item.addEventListener("click", function () {
     productsCountEl.textContent = +productsCountEl.textContent + 1;
-    console.log(productsCountEl.textContent);
+    // console.log(productsCountEl.textContent);
     return productsCountEl.textContent;
   })
 );
+
+let likeBtns = document.querySelectorAll(".like");
+for (let i = 0; i < likeBtns.length; i++) {
+  likeBtns[i].addEventListener("click", function () {
+    likeBtns[i].classList.toggle("like-active");
+  });
+}
