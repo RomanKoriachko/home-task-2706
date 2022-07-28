50649dcd6b45e2a7d830ee1f04296654448dea6c
Roman <mazaxaka.tyt@gmail.com>
2022-07-28 13:31:44 +0300

    add slider

--- app.js
+++ app.js
@@ -1,34 +1,53 @@
+// add to cart
+
 let productsCountEl = document.getElementById("products-count");
-// console.log(productsCountEl);
-// console.log(productsCountEl.textContent);
 let addToCartBtns = document.querySelectorAll(".add-to-cart-btn");
-// console.log(addToCartBtns);
-
-// addToCartBtns.onclick = function () {
-//   alert("Hello world");
-// };
-
-// addToCartBtns.addEventListener("click", function () {
-//   alert("hello world");
-// });
-
-// for (let i = 0; i < addToCartBtns.length; i++) {
-//   addToCartBtns[i].addEventListener("click", function () {
-//     console.log("clicked");
-//   });
-// }
 
 addToCartBtns.forEach((item) =>
   item.addEventListener("click", function () {
     productsCountEl.textContent = +productsCountEl.textContent + 1;
-    // console.log(productsCountEl.textContent);
-    return productsCountEl.textContent;
   })
 );
 
+// chenge like state
+
+// мое решение
+// let likeBtns = document.querySelectorAll(".like");
+// for (let i = 0; i < likeBtns.length; i++) {
+//   likeBtns[i].addEventListener("click", function () {
+//     likeBtns[i].classList.toggle("liked");
+//   });
+// }
+
+// решение на уроке
 let likeBtns = document.querySelectorAll(".like");
-for (let i = 0; i < likeBtns.length; i++) {
-  likeBtns[i].addEventListener("click", function () {
-    likeBtns[i].classList.toggle("like-active");
+
+likeBtns.forEach((item) => {
+  item.addEventListener("click", function () {
+    item.classList.toggle("liked");
   });
+});
+
+// More deteils
+let moreDeteilsBtn = document.querySelectorAll(".btn-more-deteils");
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".btn-close");
+
+moreDeteilsBtn.forEach((item) => item.addEventListener("click", openModal));
+
+function openModal() {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
 }
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+}
+
+closeBtn.addEventListener("click", closeModal);
+
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
+  });
+});
